506d69e35a469b95c358fa578630cf2f520da834
onepipfrancefinales <onepipthib@gmail.com>
2025-06-21 18:27:36 +0200

    renommage btn ANNULER

--- Phpleague/phpau/prono/Phpleague/phpal/prono - Copie/pronos1.php
+++ Phpleague/phpau/prono/Phpleague/phpal/prono - Copie/pronos1.php
@@ -412,7 +412,7 @@ $resultat=$idconnect->query("SELECT phpal_matchs.id
 
 
 <input type="hidden" name="debut" value="<?php print $debut; ?>"><br />
-<a href="index.php?page=pronos&amp;action=reset&amp;debut=<?php echo "$debut";?>&amp;gr_champ=<?php echo "$gr_champ"; ?>"><img border="0" src="reset.gif" alt=""></a>
+<a href="index.php?page=pronos&amp;action=reset&amp;debut=<?php echo "$debut";?>&amp;gr_champ=<?php echo "$gr_champ"; ?>"><img border="0" src="btn_ANNULER.gif" alt=""></a>
 <a href="javascript:ValideGrille(<?php print $x; ?>);"><img border="0" src="btn_VALIDER.gif" alt=""></a>
 
           
--- Phpleague/phppro/prono/pronos1.php
+++ Phpleague/phppro/prono/pronos1.php
@@ -391,7 +391,7 @@ if ($action == "valid_pronos") {
             <input type="hidden" name="action" value="valid_pronos">
             <input type="hidden" name="nb_fiche" value="<?php echo $x; ?>">
             <input type="hidden" name="debut" value="<?php echo $debut; ?>"><br />
-            <a href="index.php?page=pronos&amp;action=reset&amp;debut=<?php echo $debut; ?>&amp;gr_champ=<?php echo "$gr_champ"; ?>"><img border="0" src="reset.gif" alt=""></a>
+            <a href="index.php?page=pronos&amp;action=reset&amp;debut=<?php echo $debut; ?>&amp;gr_champ=<?php echo "$gr_champ"; ?>"><img border="0" src="btn_ANNULER.gif" alt=""></a>
             <a href="javascript:ValideGrille(<?php echo $x; ?>);"><img border="0" src="btn_VALIDER.gif" alt=""></a>
           </td>
         </tr>
