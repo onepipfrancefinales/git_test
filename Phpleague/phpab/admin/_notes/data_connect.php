88081620e48bf0cb22f9c1dd3e63041a3d7099e8
onepipfrancefinales <onepipthib@gmail.com>
2025-11-24 18:15:02 +0100

    config2

---
+++ Phpleague/phpab/admin/_notes/data_connect.php
@@ -0,0 +1,17 @@
+<?php
+/*
+$hostname="127.0.0.1";
+$database="onepip-france-db2";
+$login="root";
+$password="";
+*/
+$hostname="sql.franceserv.fr";
+//$database="onepip-france-db2";
+$database="onepip-france_db6";
+$login="onepip-france";
+$password="Lavelan1969&";
+
+$lang="fr";
+$PHPLEAGUE_RACINE="Phpleague/phpab/";
+$PHPLEAGUE_RACINE2="Phpleaguesmart/phpab/";
+?>
